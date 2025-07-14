3e9b66df74f8c877008e88b324f983547d6fbaab
sarun300 <s6812022610492@email.kmutnb.ac.th>
2025-07-14 16:50:18 +0700

    work

---
+++ coding_w4_03.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    char nameInitial = 'K';
+    int age = 20;
+    float weight = 48.50;
+    char gender[] = "Female";
+
+    printf("nameInitial = %c\n", nameInitial);
+    printf("age = %d\n", age);
+    printf("weight = %.2f\n", weight);
+    printf("gender = %s\n", gender);
+
+    return 0;
+}
+ 
